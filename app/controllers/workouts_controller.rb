4ec2161f2e84148e43127f74126eb54c0566673c
Morlley <morlley.hunt.3710.busi@gmail.com>
2021-07-25 00:01:41 +0900

    カレンダーを再実装

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,22 +1,52 @@
 class WorkoutsController < ApplicationController
+
   def index
-    get_two_week
+    get_calendar
   end
 
   private
 
-  def get_two_week
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-    @todays_date = Date.today
-    @two_week_days = []
+  def set_date
+    i = 74
+    @today_date = Date.today.months_ago(i)
+    @beginning_date = @today_date.beginning_of_month
+    @end_date = @today_date.end_of_month
+  end
 
-    14.times do |x|
-      wday_num = (@todays_date -10 + x).wday
-      if wday_num > 7
-        wday_num -= 7
-      end
-      days = { month: (@todays_date -10 + x).month, date: (@todays_date -10 + x).day, wday: wdays[wday_num]}
-      @two_week_days.push(days)
+  def get_this_month_days
+    this_month_days_count = @end_date.day
+    @this_month_days = []
+    this_month_days_count.times do |x|
+      day = @beginning_date + x 
+      @this_month_days.push(day)
     end
   end
+
+  def get_pre_days
+    pre_days_count = @beginning_date.wday
+    @pre_days = []
+    pre_days_count.times do |x|
+      pre_day = @beginning_date - @beginning_date.wday + x
+      @pre_days.push(pre_day)
+    end
+  end
+
+  def get_post_days
+    post_days_count = 6 - @end_date.wday
+    @post_days = []
+    post_days_count.times do |x|
+      post_day = @end_date + 1 + x
+      @post_days.push(post_day)
+    end
+  end
+
+  def get_calendar
+    set_date
+    get_this_month_days
+    get_pre_days
+    get_post_days
+
+    @calendar = @pre_days + @this_month_days + @post_days
+  end
+
 end
