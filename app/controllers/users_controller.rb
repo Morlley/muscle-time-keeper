6c0c6030d20b976747382e7d3be359bcffdb6746
Morlley <morlley.hunt.3710.busi@gmail.com>
2021-07-30 17:42:39 +0900

    他ユーザーのマイページにアクセスできないように制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
+  before_action :move_to_index
+
   def show
   end
 
   def me
-    @user = User.find(params[:id])
     @birthday = @user.birthday
+  end
+
+  private
+  
+  def move_to_index
+    @user = User.find(params[:id])
+    unless current_user.id == @user.id
+      redirect_to root_path
+    end
   end
 end
