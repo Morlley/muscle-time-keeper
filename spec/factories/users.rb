d1f3c2cc054eff35c964afbba125b8b274102d76
Morlley <morlley.hunt.3710.busi@gmail.com>
2021-07-30 18:12:36 +0900

    userモデルの単体テストコードを実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :user do
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6) + "1a"
+    password {password}
+    password_confirmation {password}
+    birthday              {Faker::Date.in_date_period}
   end
 end
