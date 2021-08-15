aa454de4c21493ad1eacae59bb46e945cf9153f3
Morlley <morlley.hunt.3710.busi@gmail.com>
2021-08-15 21:02:48 +0900

    いいね機能(非同期)の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,17 @@
 class LikesController < ApplicationController
+  before_action :set_routine
+
   def create
     Like.create(user_id: current_user.id, routine_id: params[:id])
-    redirect_to routine_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, routine_id: params[:id]).destroy
-    redirect_to routine_path
+  end
+
+  private
+
+  def set_routine
+    @routine = Routine.find(params[:id])
   end
 end
