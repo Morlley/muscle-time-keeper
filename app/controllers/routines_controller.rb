54fcecf6d869fe653e3a13c6b3abf71e5bf39105
Morlley <morlley.hunt.3710.busi@gmail.com>
2021-08-20 16:19:41 +0900

    検索結果の件数に制限を設定

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -56,7 +56,7 @@ class RoutinesController < ApplicationController
   end
 
   def search
-    @results = @q.result.where(status_id: 1).where.not(user_id: current_user.id).includes(:likes).order("created_at DESC").uniq
+    @results = @q.result(distinct: true).where(status_id: 1).where.not(user_id: current_user.id).includes(:likes).order("created_at DESC").page(params[:page]).per(15)
   end
 
   private
