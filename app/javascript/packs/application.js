cebdc9b9e7a957bc55e980ba653b7a7b46c14828
Morlley <morlley.hunt.3710.busi@gmail.com>
2021-08-25 17:06:43 +0900

    webpackerでページごとに読み込むjavascriptファイルを指定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,13 +6,9 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../move_month")
 require("../user_show")
-require("../registration_modal")
 require("jquery")
 require("@nathanvda/cocoon")
-require("../show_detail")
-require("../count_down_timer")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
