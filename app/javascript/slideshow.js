7fd5f6b4cc04abbb3117b697ed71094eae861207
Morlley <morlley.hunt.3710.busi@gmail.com>
2021-08-28 01:32:55 +0900

    ワークアウト画面の背景を調整

--- app/javascript/slideshow.js
+++ app/javascript/slideshow.js
@@ -3,7 +3,7 @@ require("./jquery.bgswitcher")
 function slideshow(){
 jQuery(function($) {
   $('.workout-new-background').bgSwitcher({
-      images: [ "/images/ehara.jpg", "/images/shimanto.jpg", '/images/sunset1.jpg', "/images/sea.jpg"],
+      images: [ "/images/ehara.jpg", "/images/shimanto.jpg", '/images/sunset2.jpg', "/images/sea1.jpg"],
       interval: 10000,
       loop: true,
       shuffle: true,
